685b5e09cc699e24bbe818e47e1c4597dbbaafe7
OcheOps <ochecodes@gmail.com>
2024-02-24 23:25:18 +0100

    Done will explain it in the readme file

--- main.go
+++ main.go
@@ -1,63 +1,107 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gocolly/colly"
 )
 
-
 type Stock struct {
-	company,price,change string 
+	company, price, change string
 }
 
-
-func main() 
-{
-
+func main(){
 	ticker := []string{
-		"AAPL",
-		"GOOGL",
-		"AMZN",
 		"MSFT",
-		"FB",
-		"TSLA",
-		"NVDA",
-		"NFLX",
-		"VZ",
-		"AMD",
-		"INTC",
 		"IBM",
-		"ORCL",}
-		stocks := []Stock{}
-			c := colly.NewCollector()
-			c.OnRequest(func(r *colly.Request){
-				println("Visiting", r.URL.String())
-	
-			})
-			c.OnError(func(r *colly.Response, err error){
-				log.println("something went wrong", err)
-	
-	
-			})
-			c.OnHTML("div#quote-header-info", func(e *colly.HTMLElement) {
-			
-				stock := Stock{}
-		
-				stock.company = e.ChildText("h1")
-				fmt.Println("Company:", stock.company)
-				stock.price = e.ChildText("fin-streamer[data-field='regularMarketPrice']")
-				fmt.Println("Price:", stock.price)
-				stock.change = e.ChildText("fin-streamer[data-field='regularMarketChangePercent']")
-				fmt.Println("Change:", stock.change)
-		
-				stocks = append(stocks, stock)
-			})
-			c.Wait()
-			for_, t := range ticker{
-				c.Visit("https://finance.yahoo.com/quote/" + t)
-	
-	
-	}
-	
-}
+		"GE",
+		"UNP",
+		"COST",
+		"MCD",
+		"V",
+		"WMT",
+		"DIS",
+		"MMM",
+		"INTC",
+  		"AXP",
+    	"AAPL",
+    	"BA",
+    	"CSCO",
+    	"GS",
+    	"JPM",
+    	"CRM",
+    	"VZ",
+		"NVDA",
+		"AMZN",
+		"GOOGL",
+		"FB",
+		"NFLX",
+		"TSLA",
+		"TSM",
+		"ORCL",
 
-	
+
+   
+	}
+
+	stocks := []Stock{}
+
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting:", r.URL)
+	})
+	c.OnError(func(_ *colly.Response, err error) { 
+		log.Println("Something went wrong: ", err) 
+	}) 
+
+	c.OnHTML("div#quote-header-info", func(e *colly.HTMLElement) {
+		
+		stock := Stock{}
+
+		stock.company = e.ChildText("h1")
+		fmt.Println("Company:", stock.company)
+		stock.price = e.ChildText("fin-streamer[data-field='regularMarketPrice']")
+		fmt.Println("Price:", stock.price)
+		stock.change = e.ChildText("fin-streamer[data-field='regularMarketChangePercent']")
+		fmt.Println("Change:", stock.change)
+
+		stocks = append(stocks, stock)
+	})
+	c.Wait()
+
+	for _, t := range ticker {
+		c.Visit("https://finance.yahoo.com/quote/" + t + "/")
+	}
+	fmt.Println(stocks)
+
+
+
+	file, err := os.Create("stocks.csv") 
+	if err != nil { 
+		log.Fatalln("Failed to create output CSV file", err) 
+	}
+
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	headers := []string{
+		"company", 
+		"price", 
+		"change", 
+	} 
+
+	writer.Write(headers) 
+	for _, stock := range stocks { 
+		record := []string{ 
+			stock.company,
+			stock.price,
+			stock.change,
+		} 
+		writer.Write(record) 
+		}
+		defer writer.Flush()
+
+}
